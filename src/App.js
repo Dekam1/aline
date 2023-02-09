50241dcc4bea103c067bed9ba94a0b26825b2ed0
Dekam <ildararslavov10@gmail.com>
2023-02-09 14:34:07 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import pismo from '../src/pismo.png'
+
+function App() {
+
+  const [open, setOpen] = React.useState(false);
+  const [pismoOpen, setPismoOpen] = React.useState(false);
+
+  const openPismo = (e) => {
+    setOpen(prev => !prev);
+  }
+
+  const test = () => {
+    if(open) {
+      setTimeout(() => {
+        setPismoOpen(true)
+      }, 2000)
+    }
+  }
+
+  test()
+
+  return (
+    <div className='main'>
+      <button onClick={e => openPismo(e)} className={open ? "main_button active" : "main_button"}>
+      </button>
+      {pismoOpen && <div className='pismo'>
+        <img src={pismo}></img>
+      </div>}
+    </div>
+  )
+}
+
+export default App;
